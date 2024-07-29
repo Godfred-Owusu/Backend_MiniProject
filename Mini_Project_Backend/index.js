fca682b9dbc6ff9912dddccb45806995ce3810ef
Godfred-Owusu <gowusu018@gmail.com>
2024-07-30 00:49:26 +0100

    Created a tested route

--- Mini_Project_Backend/index.js
+++ Mini_Project_Backend/index.js
@@ -12,6 +12,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/books", bookRoutes);
 app.use("/api/students", studentRoutes);
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
